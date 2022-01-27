0cf051baabf4c39b15969159844e68a64935ee74
Anastasia Medentseva <ms.suprex@mail.ru>
2022-01-27 17:24:27 +0700

    add global actions modals, toasts, loader

--- mixins/global.js
+++ mixins/global.js
@@ -1,7 +1,14 @@
 import Vue from 'vue';
+import { mapActions } from 'vuex';
 
 Vue.mixin({
   methods: {
+    ...mapActions({
+      ShowModal: 'modals/show',
+      CloseModal: 'modals/hide',
+      SetLoader: 'loader/setLoading',
+      ShowToast: 'modals/showToast',
+    }),
     GetShortAddress(address) {
       if (!address) {
         return '';
--- store/modals/actions.js
+++ store/modals/actions.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 export default {
   show({ commit }, payload) {
     commit('setIsShow', true);
@@ -11,5 +12,16 @@ export default {
   },
   setUnclosable({ commit }) {
     commit('setUnclosable');
+  },
+  showToast(_, value) {
+    const codeText = value?.code ? $nuxt.$t(`toasts.errors.${value.code}`) : $nuxt.$t('toasts.errors.0');
+    this._vm.$bvToast.toast(value?.text || codeText, {
+      title: value.title || $nuxt.$t('toasts.title'),
+      variant: value.variant || 'danger',
+      solid: true,
+      toaster: value.toaster || 'b-toaster-bottom-right',
+      appendToast: value.appendToast || false,
+      autoHideDelay: value.delay || 5000,
+    });
   },
 };
